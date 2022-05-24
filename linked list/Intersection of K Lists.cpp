de39fa6c52a8bd52bc85f58611efc6a03f7619cf
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-05-24 16:21:39 +0530

    Create Intersection of K Lists.cpp

---
+++ linked list/Intersection of K Lists.cpp
@@ -0,0 +1,95 @@
+// https://www.learning.algozenith.com/problems/Intersection-of-K-Lists-520
+
+/*
+
+Description
+
+Given the heads of K singly-linked lists, return their common intersection node. If the linked lists don’t intersect, return null. It is guaranteed that this structure does not have any cycles.
+
+Complete the function
+
+ListNode* getKIntersectionNode(vector<ListNode*> head)
+
+
+Input Format
+
+The first line of input contains K - the number of linked lists.
+The 2ith line contains Ni - the number of nodes in ith linked list.
+The (2i + 1)th line contains Ni space-separated integers denoting the nodes in the ith linked list.
+Each unique node has a unique value, and vice-versa. No two different nodes have the same value.
+
+The following function parses the input
+
+vector<ListNode*> GetList(vector<vector<int>> num, int K)
+
+
+Constraints
+
+1 ≤ K ≤ 100
+0 ≤ Ni ≤ 1000
+
+
+Sample Input 1
+
+3
+7
+1 2 3 5 6 8 9
+6
+4 3 5 6 8 9
+4
+7 6 8 9
+
+Sample Output 1
+
+6
+
+Note
+
+Node 6 is a common intersection of all linked lists.
+
+*/
+
+
+ListNode * intersection(int i, int j,vector<ListNode *>&lists) {
+    ListNode* it1=lists[i],*it2=lists[j];
+    while(it1 and it2){
+        it1=it1->next; 
+        it2=it2->next;
+    }
+    ListNode* inter;
+    if(it1){
+        inter=lists[i];
+        while(it1){
+            it1=it1->next;
+            inter=inter->next;
+        }
+        it2=lists[j];
+        while(inter!=it2){
+            inter=inter->next;
+            it2=it2->next;
+        }
+    }
+    else{
+        inter=lists[j];
+        while(it2){
+            it2=it2->next;
+            inter=inter->next;
+        }
+        it1=lists[i];
+        while(inter!=it1){
+            inter=inter->next;
+            it1=it1->next;
+        }
+    }
+    return inter;
+}
+
+ListNode* getKIntersectionNode(vector<ListNode*>& lists) {
+    if(lists.size()<2) return lists.front();
+    ListNode* ans;
+    for(int i=1;i<lists.size(); i++){
+        ans=intersection(i-1,i,lists);
+        lists[i]=ans;
+    }
+    return ans;
+}
